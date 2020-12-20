c2977467f6b55901c1a26ff2a55405203022d452
Hyeonseo Yang <jourhyang123@gmail.com>
2020-12-21 08:16:53 +0900

    :tada: Start project

--- jni/hello.cpp
+++ jni/hello.cpp
@@ -1,6 +1,36 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
 
-int main()
+#include <sys/stat.h>
+#include <unistd.h>
+#include <pwd.h>
+#include <grp.h>
+
+int listdir(const char *path)
 {
-	std::cout << "Hello World!" << std::endl;
+    struct dirent *entry; 
+    DIR *dp;
+    dp = opendir(path);
+    if (dp == NULL) 
+    { 
+        perror("opendir");
+        return -1; 
+    } 
+    while((entry = readdir(dp)))
+    {
+        puts(entry->d_name);
+        printf("%d\r\n",entry->d_type);
+    }
+    closedir(dp); 
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    listdir(argv[1]);
+    return 0;
 }
